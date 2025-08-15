98d65e3eaa4144f8b388c9e58cdf7b6e9c180fe7
osein607 <osein607@naver.com>
2025-08-15 22:13:01 +0900

    feature/#16: 프론트 로컬 주소 CORS 설정

--- project/settings.py
+++ project/settings.py
@@ -167,6 +167,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
   "http://127.0.0.1:8000",
   "https://127.0.0.1:8000",
+  "http://localhost:5173",
 
   "https://sein0327.shop",
 ]
